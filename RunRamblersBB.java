9bfc0a0f173d887aa98344a64c08945e78e2eaeb
PPPPPPPPeter <65163220+PPPPPPPPeter@users.noreply.github.com>
2021-05-17 15:54:36 +0100

    Add files via upload

---
+++ RunRamblersBB.java
@@ -0,0 +1,11 @@
+
+public class RunRamblersBB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TerrainMap map1 = new TerrainMap("src/tmc.pgm");
+		
+		
+	}
+
+}
