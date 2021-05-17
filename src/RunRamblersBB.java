73ab362604aa8c19e558b4adbd77d531b2dfa753
PPPPPPPPeter <65163220+PPPPPPPPeter@users.noreply.github.com>
2021-05-17 16:26:25 +0100

    using the random the create 10 paths to test the BB.
    using the random the create 10 paths(11) to test the BB.

--- src/RunRamblersBB.java
+++ src/RunRamblersBB.java
@@ -4,7 +4,21 @@ public class RunRamblersBB {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TerrainMap map1 = new TerrainMap("src/tmc.pgm");
-		
+		Random random = new Random();
+		ArrayList<Coords> goals = new ArrayList<>();
+		int num = 10;
+		while (num >= 0) {
+		    int x = Math.abs(random.nextInt()) % map1.getWidth();
+		    int y = Math.abs(random.nextInt()) % map1.getDepth();
+		    if (x == 0 && y == 0) {
+			continue;
+		    }
+		    Coords coords = new Coords(y, x);
+		    if (!inGoals(coords, goals)) {
+			goals.add(coords);
+			num--;
+		    }
+		}
 		
 	}
 
