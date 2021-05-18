b984fcc6b763e6dd2bcfa09f7f989c810adb77f3
PPPPPPPPeter <65163220+PPPPPPPPeter@users.noreply.github.com>
2021-05-18 23:00:15 +0100

    add some comments for using different values in AStar

--- src/RamblersSearch.java
+++ src/RamblersSearch.java
@@ -4,7 +4,17 @@ public class RamblersSearch extends Search{
 	
 	private TerrainMap map;
 	private Coords goal;
+	/*
+	     * this static variable is used to test the efficiency of different estRemCost in Astar.
+	     * the String variables options:
+	     * the Manhattan Distance
+	     * the Euclidean distance
+	     * the height difference
+	     */
     	public static final String heuristicsMethod = "Other";
+	//public static final String heuristicsMethod = "MD";
+	//public static final String heuristicsMethod = "ED";
+	//public static final String heuristicsMethod = "HD";
 	
 	public TerrainMap getTerrainMap() {
 		return this.map;
